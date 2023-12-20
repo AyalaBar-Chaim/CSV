f2b2d9f5d57b9c3674c284241f4a7d28ca74fc34
AyalaBar-Chaim <AYALAG7521@GMAIL.COM>
2023-12-20 20:56:33 +0200

    Initial commit

---
+++ CSVTask/CSV.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayalas_Project
+{
+    public class CSV
+    {
+        public List<List<object>> GenerateData(List<string> femaleFirstNames, List<string> maleFirstNames, List<string> lastNames, int rowCount)
+        {
+            List<List<object>> data = new List<List<object>>();
+            Random random = new Random();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                string firstName = (random.Next(2) == 0) ? femaleFirstNames[random.Next(femaleFirstNames.Count)] : maleFirstNames[random.Next(maleFirstNames.Count)];
+                string lastName = lastNames[random.Next(lastNames.Count)];
+                int age = random.Next(18, 71);
+                double weight = Math.Round(random.NextDouble() * (200 - 100) + 100, 2); // Generate weight in lbs
+                string gender = (firstName == femaleFirstNames[random.Next(femaleFirstNames.Count)]) ? "Female" : "Male";
+
+                List<object> person = new List<object> { firstName, lastName, age, weight, gender };
+                data.Add(person);
+            }
+
+            return data;
+        }
+
+        public void WriteToCsv(string filePath, string[] headers, List<List<object>> data)
+        {
+            using (StreamWriter file = new StreamWriter(filePath))
+            {
+                file.WriteLine(string.Join(",", headers));
+
+                foreach (var row in data)
+                {
+                    file.WriteLine(string.Join(",", row));
+                }
+            }
+        }
+
+        public void PrintCsvContents(string filePath)
+        {
+            Console.WriteLine("Contents of the CSV file:");
+            var csvLines = File.ReadAllLines(filePath);
+            foreach (var line in csvLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
---
+++ CSVTask/Program.cs
@@ -0,0 +1,50 @@
+﻿using Ayalas_Project;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        CSV csv = new CSV();
+        List<string> femaleFirstNames = new List<string> { "Ayala", "Miriam", "Tova", "Michal", "Noa", "Shira", "Tamar", "Hadas" };
+        List<string> maleFirstNames = new List<string> { "Naftali", "Avi", "David", "Yonatan", "Tzvi", "Yakov", "Asher", "Nachum" };
+        List<string> lastNames = new List<string> { "Smith", "Johnson", "Williams", "Jones", "Brown", "Miller", "Davis", "Garcia", "Rodriguez", "Martinez" };
+
+        string[] headers = { "FirstName", "LastName", "Age", "Weight", "Gender" };
+        string filePath = "persons_details.csv";
+
+        // Generate data and write to CSV
+        List<List<object>> data = csv.GenerateData(femaleFirstNames, maleFirstNames, lastNames, 1000);
+        csv.WriteToCsv(filePath, headers, data);
+
+        Console.WriteLine("CSV file created successfully.\n");
+
+        // Print the contents of the CSV file
+        //PrintCsvContents(filePath);
+
+        // Read and analyze CSV data
+        var lines = File.ReadAllLines(filePath).Skip(1); // Skip header
+        var parsedData = lines.Select(line => line.Split(',')).Select(fields => new
+        {
+            FirstName = fields[0],
+            LastName = fields[1],
+            Age = int.Parse(fields[2]),
+            Weight = double.Parse(fields[3]),
+            Gender = fields[4]
+        }).ToList();
+
+        // Calculate and print statistics
+        double averageAge = parsedData.Average(person => person.Age);
+        Console.WriteLine($"\nAverage age of all people: {averageAge:F2} years");
+
+        int countInRange = parsedData.Count(person => person.Weight >= 120 && person.Weight <= 140);
+        Console.WriteLine($"Total number of people weighing between 120lbs and 140lbs: {countInRange}");
+
+        double averageAgeInRange = parsedData.Where(person => person.Weight >= 120 && person.Weight <= 140).Average(person => person.Age);
+        Console.WriteLine($"Average age of people weighing between 120lbs and 140lbs: {averageAgeInRange:F2} years");
+    }
+}
+
